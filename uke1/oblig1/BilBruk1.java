6fbe1aa9335c77fc5844b4d19c97fb08bd002930
Kevin <kevinalexander@live.no>
2023-01-26 16:39:47 +0100

    oblig0op1 - ferdig

---
+++ uke1/oblig1/BilBruk1.java
@@ -0,0 +1,8 @@
+public class BilBruk1 {
+    
+    public static void main(String[] args){
+        
+        Bil1 nyBil1 = new Bil1();   // Oppretter bilobjekt
+        nyBil1.bilMelding();    // Benytter skriv-metoden fra objektet
+    }
+}
