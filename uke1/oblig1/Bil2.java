4266e1339d921f00fecd27a838152b23ede51b10
Kevin <kevinalexander@live.no>
2023-01-26 16:58:04 +0100

    op2 ferdig

---
+++ uke1/oblig1/Bil2.java
@@ -0,0 +1,12 @@
+public class Bil2 {
+
+    String bilNum;
+    
+    public Bil2(){
+        bilNum = "DN82643";
+    }
+
+    public void bilMelding(){
+        System.out.println("Jeg er en bil med bilnummer " + bilNum);
+    }
+}
---
+++ uke1/oblig1/BilBruk2.java
@@ -0,0 +1,8 @@
+public class BilBruk2 {
+    
+    public static void main(String[] args){
+        
+        Bil2 nyBil1 = new Bil2();   // Oppretter bilobjekt
+        nyBil1.bilMelding();    // Benytter skriv-metoden fra objektet
+    }
+}
