4f89f851a30e5890d4a05bd1569d713487b64e3f
Kevin Aslesen <kevinalexander@live.no>
2023-02-21 13:21:39 +0100

    ey

---
+++ uke4/trix/TestAB.java
@@ -0,0 +1,9 @@
+public class TestAB {
+    public static void main(String[] args) {
+        B peker1 = new B();
+        A peker2 = peker1;
+
+        peker1.skrivKlasse();
+        peker2.skrivKlasse();
+    }
+}
---
+++ uke4/trix/TestBygning.java
@@ -0,0 +1,5 @@
+public class TestBygning {
+    public static void main(String[] args) {
+        Skyskraper nySkyskraper = new Skyskraper(3.21, 3);
+    }
+}
